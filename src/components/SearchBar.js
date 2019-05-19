74d58c80da303fe64b0ce05869f5ac26a92c9bb9
johnpospisil <audiofreak7@gmail.com>
2019-05-19 09:53:42 -0700

    Connect a function to form submital using 'onSubmit'

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,17 +3,20 @@ import React from 'react';
 class SearchBar extends React.Component {
 state = { term: ''};
 
-onInputChange = () => {
-
+onInputChange = (event) => {
+  this.setState({ term: event.target.value });
 };
+
+onFormSubmit = event => {
+  event.preventDefault(); // prevent the browser from refreshing when ENTER is pressed
+  // TODO: Make sure we call callback from parent component
+}
 
   render() {
     return <div className="search-bar ui segment">
-      <form className="ui form">
+      <form onSubmit={this.onFormSubmit} className="ui form">
         <div className="filed">
           <label>Video Search</label>
-          {/* Change from an uncontrolled input to a controlled input. */}
-          {/* This way data will be saved in the component, and not in the DOM. */}
           <input type="text" value={this.state.term} onChange={this.onInputChange}></input>
         </div>
       </form>
