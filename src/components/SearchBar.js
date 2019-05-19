946233d7815787284fb5b9f1e7246c940b55129f
johnpospisil <audiofreak7@gmail.com>
2019-05-19 09:31:58 -0700

    Add Semantic UI, and a form element in the SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    return <div>SearchBar</div>;
+    return <div className="search-bar ui segment">
+      <form className="ui form">
+        <div className="filed">
+          <label>Video Search</label>
+          <input type="text"></input>
+        </div>
+      </form>
+    </div>;
   }
 }
 
