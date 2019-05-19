9dc61474b81d4e87f5bbeb8137e90571c6037b9b
johnpospisil <audiofreak7@gmail.com>
2019-05-19 15:06:57 -0700

    Install axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,3 +1,15 @@
-// get this API Key by setting up a project and getting
-// credentials from https://console.developers.google.com/apis
+// remember to do a 'npm install axios' in your terminal
+// to install axios into your project
+import axios from 'axios';
+
 const KEY = 'AIzaSyCOX_g_sFUhUWGJZhd0JlY2Vpzf4AVBukr';
+
+// https://developers.google.com/youtube/v3/docs/search/list
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3/',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+});
