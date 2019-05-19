f9f658ebe9af14b4f155e1bf3b8fe11f8bb9194d
johnpospisil <audiofreak7@gmail.com>
2019-05-19 09:42:42 -0700

    The SearcBar 'input' element is no a controlled input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+state = { term: ''};
+
+onInputChange = () => {
+
+};
+
   render() {
     return <div className="search-bar ui segment">
       <form className="ui form">
         <div className="filed">
           <label>Video Search</label>
-          <input type="text"></input>
+          {/* Change from an uncontrolled input to a controlled input. */}
+          {/* This way data will be saved in the component, and not in the DOM. */}
+          <input type="text" value={this.state.term} onChange={this.onInputChange}></input>
         </div>
       </form>
     </div>;
