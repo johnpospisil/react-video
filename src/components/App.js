cf4510e563c314ddb92585a4bda0f43ffbb901a9
johnpospisil <audiofreak7@gmail.com>
2019-05-25 12:26:32 -0700

    Create the 'VideoList' component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-  state = {videos: []}; // When App is created, 'this.state.videos' property is
-  // set to be empty
+  state = {videos: []}; 
 
   onTermSubmit = async term => { // when a user submits a search term, do this...
-    // console.log(term);
     const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
-    // console.log(response);
-    // set 'this.state.videos' to the response.data.items from the youtube response
     this.setState({ videos: response.data.items });
   };
 
@@ -22,7 +19,7 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit}/>
-        I have {this.state.videos.length} videos.
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
---
+++ src/components/VideoList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const VideoList = props => {
+  
+  return <div>Number of returned videos: {props.videos.length}</div>;
+};
+
+export default VideoList;
