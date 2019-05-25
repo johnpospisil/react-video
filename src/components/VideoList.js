dab8fcce3228815af0065aefa04d43f3e2fee45c
johnpospisil <audiofreak7@gmail.com>
2019-05-25 12:38:50 -0700

    Create the 'VideoItem' component, and render a list of videos

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = props => {
+const VideoList = ({ videos }) => {
+  const renderedList = videos.map((video) => {
+    return <VideoItem />;
+  });
   
-  return <div>Number of returned videos: {props.videos.length}</div>;
+  return <div>{renderedList}</div>;
 };
 
 export default VideoList;
