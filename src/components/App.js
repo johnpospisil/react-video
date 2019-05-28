54c859e15a8111f403b7398f8487c3644feb7d8c
johnpospisil <audiofreak7@gmail.com>
2019-05-28 16:17:11 -0700

    Show the first video in the VideoDetail, and search for 'news' by default.

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,18 @@ import youtube from '../apis/youtube';
 class App extends React.Component {
   state = { videos: [], selectedVideo: null }; 
 
+  // when the App component mounts, search youtube for 'news'
+  componentDidMount() {
+    this.onTermSubmit('news');
+  }
+
   onTermSubmit = async term => { // when a user submits a search term, do this...
     const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
-    this.setState({ videos: response.data.items });
+    this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
   };
 
   onVideoSelect = video => {
