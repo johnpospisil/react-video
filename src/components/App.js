93cf749121aa8632c4cc77a3f5430f16e58a7c78
johnpospisil <audiofreak7@gmail.com>
2019-05-28 10:07:29 -0700

    Create the 'VideoDetail' component which features the video clicked by the user

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
@@ -16,7 +17,8 @@ class App extends React.Component {
   };
 
   onVideoSelect = video => {
-    console.log("From the App!", video);
+    // console.log("From the App!", video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
@@ -25,6 +27,7 @@ class App extends React.Component {
         {/* The prop and callback do not have to have the same name, but usually do. */}
         {/* See below fro examples */}
         <SearchBar onFormSubmit={this.onTermSubmit}/>
+        <VideoDetail  video={this.state.selectedVideo}/>
         <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
       </div>
     );
